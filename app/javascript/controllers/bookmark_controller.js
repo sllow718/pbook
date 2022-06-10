c27551cdbb67545238188d59513254403c40f010
Minquan Tan <minch_tan@hotmail.com>
2022-06-10 16:47:48 +0800

    created js controller bookmark

---
+++ app/javascript/controllers/bookmark_controller.js
@@ -0,0 +1,21 @@
+import { Controller } from "stimulus"
+
+export default class extends Controller {
+  static targets = [ "bookmarkIcon" ]
+
+  connect() {
+    // this.outputTarget.textContent = 'Hello, Stimulus!'
+    console.log('time for bookmarking')
+  }
+
+
+  bookmark() {
+    this.bookmarkIconTarget.innerText = "Bingo!"
+    console.log('k')
+    // get the item that has been clicked
+    // get the user who clicked
+    // change the favourited status of the item
+    // link the clicker to the item
+
+  }
+}
