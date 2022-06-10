068fa59332f04eeb9c0a1f103185a99c4ba3bef3
Minquan Tan <minch_tan@hotmail.com>
2022-06-10 20:47:13 +0800

    removed stimulus. Set icon color with conditional 'bookmark.present?'

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -13,18 +13,4 @@ class BookmarksController < ApplicationController
     end
     redirect_to stall_path(dish.stall.id)
   end
-
-    # new_bookmark = Bookmark.new
-    # new_bookmark.dish_id = dish.id
-    # new_bookmark.user_id = user.id
-    # if new_bookmark.save
-    # end
-
-  # def destroy
-  #   dish = Dish.find(params[:dish_id])
-  #   user = current_user
-  #   @bookmark = Bookmark.where(user_id: user.id, dish_id: dish.id)
-  #   @bookmark.destroy
-  #   redirect_to stall_path(dish.stall.id)
-  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,6 @@ Rails.application.routes.draw do
   resources :dishes do
     get 'bookmarks', to: 'bookmarks#bookmark'
   end
-  # resources :bookmarks, only: [:destroy]
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
