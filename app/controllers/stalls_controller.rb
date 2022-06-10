c9257af5bccd04e2ccbe2a3027fbb7dd336c869c
Minquan Tan <minch_tan@hotmail.com>
2022-06-10 15:15:04 +0800

    cloudinary up, new stall creation now possible

--- app/controllers/stalls_controller.rb
+++ app/controllers/stalls_controller.rb
@@ -10,12 +10,27 @@ class StallsController < ApplicationController
 
   def new
     @stall = Stall.new
-    @hawker_center_names = []
-    HawkerCenter.all.each do |hawker_center|
-      @hawker_center_names << hawker_center.name
-    end
+    @hawkers = HawkerCenter.all
+    # @hawker_center_names = []
+    # HawkerCenter.all.each do |hawker_center|
+    #   @hawker_center_names << hawker_center.name
+    # end
   end
 
   def create
+    @stall = Stall.new(stall_params)
+    @stall.user_id = current_user.id
+
+    if @stall.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def stall_params
+    params.require(:stall).permit(:name, :unit, :hawker_center_id, :description, :photo)
   end
 end
