0806624893127a5953a4afa420584d5ef9681974
poh mingfu <718pohmf@gmail.com>
2022-06-11 14:52:18 +0800

    added support for local host testing for bookmarks

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -2,15 +2,18 @@ class BookmarksController < ApplicationController
   def bookmark
     dish = Dish.find(params[:dish_id])
     user = current_user
-    bookmark = Bookmark.where(user_id: user.id, dish_id: dish.id).first
+    bookmark = Bookmark.where(user_id: user.id, dish_id: dish.id).first if user.id rescue false
     if bookmark.present?
       Bookmark.destroy(bookmark.id)
-    else
+      redirect_to stall_path(dish.stall.id)
+    elsif (user.id rescue false)
       new_bookmark = Bookmark.new
       new_bookmark.dish_id = dish.id
       new_bookmark.user_id = user.id
       new_bookmark.save!
+      redirect_to stall_path(dish.stall.id)
+    else
+      redirect_to stall_path(dish.stall.id), alert: "You must be logged in to bookmark"
     end
-    redirect_to stall_path(dish.stall.id)
   end
 end
