df69febbb03a6d9566aa6d90c1d5ea3a3304dd9d
Minquan Tan <minch_tan@hotmail.com>
2022-06-12 21:00:46 +0800

    partial ok except for big card

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -2,7 +2,7 @@ class BookmarksController < ApplicationController
   def bookmark
     dish = Dish.find(params[:dish_id])
     user = current_user
-    raise
+    # raise
     bookmark = Bookmark.where(user_id: user.id, dish_id: dish.id).first if user.id rescue false
     if bookmark.present?
       Bookmark.destroy(bookmark.id)
