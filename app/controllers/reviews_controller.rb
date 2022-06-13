98832e694674d7de71fbefe4074aef0820f61474
justinn leong <justinnjamesleong@gmail.com>
2022-06-13 15:41:35 +0800

    review done with rating

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,29 +1,28 @@
 class ReviewsController < ApplicationController
-   def create
-    @dish = Dish.find(params[:dish_id])
-    @review = Review.new(review_params)
-    @review.dish = @dish
-
-    respond_to do |format|
-      if  @dish.save
-        format.html { redirect_to dish_path(@dish, anchor: "review-#{@review.id}") }
-        format.json
-      else
-        format.html { render 'dishes/show', status: :unprocessable_entity }
-        format.json
-      end
-    end
-  end
-
   def new
     @dish = Dish.find(params[:dish_id])
     @review = Review.new
   end
 
+  def create
+    @dish = Dish.find(params[:dish_id])
+    @review = Review.new(review_params)
+    @review.user = current_user
+    @review.dish = @dish
+
+  if @review.save
+      redirect_to @dish
+    else
+      render @review
+    end
+  end
+
+
+
 
   private
 
   def review_params
-    params.require(:review).permit(:comment, :photo)
+    params.require(:review).permit(:comment, :photo, :rating)
   end
 end
