03d285932e4829fc238d83bf0540a1ba93943e51
justinn leong <justinnjamesleong@gmail.com>
2022-06-14 14:09:29 +0800

    flavor added

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,27 +2,35 @@ class ReviewsController < ApplicationController
   def new
     @dish = Dish.find(params[:dish_id])
     @review = Review.new
+    @review_flavor = ReviewFlavor.new
+    @flavors = Flavor.all
   end
 
   def create
     @dish = Dish.find(params[:dish_id])
-    @review = Review.new(review_params)
+    new_review_params = review_params
+    new_review_params.delete("review_flavor")
+    @review = Review.new(new_review_params)
+
     @review.user = current_user
     @review.dish = @dish
-
-  if @review.save
-      redirect_to @dish
+    if @review.save
+      new_review_flavor_params =  review_params["review_flavor"]
+      new_review_flavor_params["review_id"] = @review.id
+      @review_flavor = ReviewFlavor.new(new_review_flavor_params)
+      if @review_flavor.save
+        redirect_to @dish
+      else
+        render @review
+      end
     else
       render @review
     end
   end
 
-
-
-
   private
 
   def review_params
-    params.require(:review).permit(:comment, :photo, :rating)
+    params.require(:review).permit(:comment, :photo, :rating, :review_flavor => [:flavor_id])
   end
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,5 @@ class Review < ApplicationRecord
   belongs_to :user
   belongs_to :dish
   has_one_attached :photo
+  has_many :review_flavors
 end
