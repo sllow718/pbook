fe0765a3ae4c8113abd1542ff4275e9c7da03644
Minquan Tan <minch_tan@hotmail.com>
2022-06-14 14:28:57 +0800

    comment out container, generate relevant dishes in sidebar

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,5 +1,6 @@
 class DashboardsController < ApplicationController
   def dashboard
-
+    @dashboard_stall = Stall.find_by(user_id: current_user.id)
+    @dishes = @dashboard_stall.dishes
   end
 end
