b1b5afec6aaed58fe52e686338dcefa5a42ba381
poh mingfu <718pohmf@gmail.com>
2022-06-15 14:09:26 +0800

    halfway completed for map integration for stalls

--- app/javascript/controllers/search_food_controller.js
+++ app/javascript/controllers/search_food_controller.js
@@ -4,12 +4,7 @@ export default class extends Controller {
   static targets = ["form", "input", "list", "topgrid"];
 
   connect() {
-    console.log(this.formTarget);
-    console.log(this.inputTarget);
-    console.log(this.listTarget);
     console.log("Connected");
-    console.log(this.topgridTarget.classList);
-    console.log(this.bottomgridTarget.classList);
   }
 
   update(event) {
