416addbf7901b8205cac6b10fd9a63c18de36d8f
justinn leong <justinnjamesleong@gmail.com>
2022-06-16 14:02:29 +0800

    will come back

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -7,6 +7,19 @@ class DishesController < ApplicationController
     @dish = Dish.find(params[:id])
     @stall = @dish.stall
     @review = Review.new
+    flavorsarray=[]
+    @dish.reviews.each do |review|
+      review.review_flavors.each do |review_flavor|
+        flavorsarray<<review_flavor.flavor.name
+      end
+    end
+    flavor_count = Hash.new(0)
+    flavorsarray.each {|flavor| flavor_count[flavor] += 1}
+    @last_flavor = flavor_count.sort_by { |flavor,number| number}
+    raise
+flavor_count.delete(@last_flavor)
+             @second_last_flavor=flavor_count.sort_by { |flavor,number| number}.last[0]
+
   end
 
   def new
