178f3f355ff45083110e691889921c8c01a2040d
justinn leong <justinnjamesleong@gmail.com>
2022-06-17 15:02:20 +0800

    save something

---
+++ app/javascript/controllers/reviewmodal_controller.js
@@ -0,0 +1,16 @@
+import { Controller } from "@hotwired/stimulus";
+
+export default class extends Controller {
+  static targets = ["form", "lists"];
+
+  connect() {
+    console.log("You are connected to the review page.");
+  }
+
+  open() {
+    this.listsTarget.classList.toggle("d-none");
+    this.formTarget.classList.toggle("d-none");
+    // this.popUpTarget.classList.remove("fade");
+    // render "reviewform"
+  }
+}
