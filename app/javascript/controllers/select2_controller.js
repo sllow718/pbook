89fcdc6fe91a84c4673821fbacf60bd61ded611e
justinn leong <justinnjamesleong@gmail.com>
2022-06-20 11:06:34 +0800

    Cleanup input

--- app/javascript/controllers/select2_controller.js
+++ app/javascript/controllers/select2_controller.js
@@ -6,6 +6,7 @@ import "select2/dist/css/select2.min.css";
 export default class extends Controller {
   static values = { options: Object };
   connect() {
+    console.log("Select 2 connected");
     $(this.element).select2(this.optionsValue);
   }
 }
